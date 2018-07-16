ecd8924dcd0f78aed36fbd061dabddb5f90e37a7
Doug Swisher <big.swish@gmail.com>
2018-07-16 18:18:55 -0500

    ch3: rays, simple camera, background

--- src/vec3.h
+++ src/vec3.h
@@ -18,9 +18,38 @@ public:
 
     // TODO - operators
 
+    inline float length() const
+    {
+        return sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]);
+    }
+
     // TODO - other methods
 
 private:
     float e[3];
 };
+
+
+inline vec3 operator+(const vec3 &v1, const vec3 &v2)
+{
+    return vec3(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v2.e[2]);
+}
+
+
+inline vec3 operator*(float t, const vec3 &v)
+{
+    return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
+}
+
+
+inline vec3 operator/(vec3 v, float t)
+{
+    return vec3(v.e[0] / t, v.e[1] / t, v.e[2] / t);
+}
+
+
+inline vec3 unit_vector(vec3 v)
+{
+    return v / v.length();
+}
 
