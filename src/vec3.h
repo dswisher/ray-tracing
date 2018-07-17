67e0708d3d67c3cdaf26e3900862e6f204333804
Doug Swisher <big.swish@gmail.com>
2018-07-16 19:41:52 -0500

    ch4: adding a sphere

--- src/vec3.h
+++ src/vec3.h
@@ -42,6 +42,12 @@ inline vec3 operator+(const vec3 &v1, const vec3 &v2)
 }
 
 
+inline vec3 operator-(const vec3 &v1, const vec3 &v2)
+{
+    return vec3(v1.x() - v2.x(), v1.y() - v2.y(), v1.z() - v2.z());
+}
+
+
 inline vec3 operator*(float t, const vec3 &v)
 {
     return vec3(t * v.x(), t * v.y(), t * v.z());
@@ -51,6 +57,12 @@ inline vec3 operator*(float t, const vec3 &v)
 inline vec3 operator/(vec3 v, float t)
 {
     return vec3(v.x() / t, v.y() / t, v.z() / t);
+}
+
+
+inline float dot(const vec3 &v1, const vec3 &v2)
+{
+    return v1.x() * v2.x() + v1.y() * v2.y() + v1.z() * v2.z();
 }
 
 
