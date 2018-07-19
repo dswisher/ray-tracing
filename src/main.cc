cca6f2394e1d591f163cc72431c39740e36a85b1
Doug Swisher <big.swish@gmail.com>
2018-07-19 06:58:44 -0500

    ch7: diffuse materials

--- src/main.cc
+++ src/main.cc
@@ -6,13 +6,30 @@
 #include "hitable_list.h"
 #include "float.h"
 
+// TODO - figure out a better way to make randomness available - globals are icky
+std::mt19937 chaos(316);    // use a set seed to be deterministic
+std::uniform_real_distribution<> dist(0.0, 1.0);
+
+
+vec3 random_in_unit_sphere()
+{
+    vec3 p;
+    do
+    {
+        p = 2.0 * vec3(dist(chaos), dist(chaos), dist(chaos)) - vec3(1, 1, 1);
+    } while (p.squared_length() > 1.0);
+
+    return p;
+}
+
 
 vec3 color(const ray& r, hitable *world)
 {
     hit_record rec;
-    if (world->hit(r, 0.0, MAXFLOAT, rec))
+    if (world->hit(r, 0.001, MAXFLOAT, rec))
     {
-        return 0.5 * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+        vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+        return 0.5 * color(ray(rec.p, target - rec.p), world);
     }
 
     vec3 unit_direction = unit_vector(r.direction());
@@ -22,8 +39,6 @@ vec3 color(const ray& r, hitable *world)
 
 
 int main() {
-    std::mt19937 chaos(316);    // use a set seed to be deterministic
-    std::uniform_real_distribution<> dist(0.0, 1.0);
 
     int nx = 200;
     int ny = 100;
@@ -51,6 +66,7 @@ int main() {
             }
 
             col /= float(ns);
+            col = vec3(sqrt(col.r()), sqrt(col.g()), sqrt(col.b()));
 
             int ir = int(255.99 * col.r());
             int ig = int(255.99 * col.g());
